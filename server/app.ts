8a44191e9d0ce635de0b63f06f3e1347ff62c297
Camden Bickel <cam@bickel.io>
2020-01-23 22:46:39 -0500

    add favicons

--- server/app.ts
+++ server/app.ts
@@ -4,16 +4,46 @@ import path from 'path';
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
 app.use(
   '/static',
   express.static(path.resolve(__dirname, '../client/build/static/'))
 );
 
-app.get('/app', (req, res) => {
+app.use(
+  '/favicon.ico',
+  express.static(path.resolve(__dirname, '../client/build/favicon.ico'))
+);
+
+app.use(
+  '/favicon-32x32.png',
+  express.static(path.resolve(__dirname, '../client/build/favicon-32x32.png'))
+);
+
+app.use(
+  '/favicon-64x64.png',
+  express.static(path.resolve(__dirname, '../client/build/favicon-64x64.png'))
+);
+
+app.use(
+  '/site.webmanifest',
+  express.static(path.resolve(__dirname, '../client/build/site.webmanifest'))
+);
+
+app.use(
+  '/apple-touch-icon.png',
+  express.static(
+    path.resolve(__dirname, '../client/build/apple-touch-icon.png')
+  )
+);
+
+app.use(
+  '/safari-pinned-tab.svg',
+  express.static(
+    path.resolve(__dirname, '../client/build/safari-pinned-tab.svg')
+  )
+);
+
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
