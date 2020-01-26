1992c31604ab45b250023c28cc27e1835c457865
Camden Bickel <cam@bickel.io>
2020-01-25 21:35:25 -0500

    add router

---
+++ client/src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '../home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
