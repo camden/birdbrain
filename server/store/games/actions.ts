9e7549cd9ac9ca7c10cd003cd038a95b52252b89
Camden Bickel <cam@bickel.io>
2020-02-06 10:13:46 -0500

    add initial game reducer/action (altho this structure is not exactly correct)

--- server/store/games/actions.ts
+++ server/store/games/actions.ts
@@ -1,1 +1,10 @@
-export const dummy = () => {}; // this is only to prevent tsc from complaining
+import { SET_CURRENT_GAME, SetCurrentGameAction, GameID } from './types';
+
+export const setCurrentGame = (game: GameID): SetCurrentGameAction => {
+  return {
+    type: SET_CURRENT_GAME,
+    payload: {
+      game,
+    },
+  };
+};
--- server/store/games/reducers.ts
+++ server/store/games/reducers.ts
@@ -1,9 +1,19 @@
-import { GameState, GameActionTypes } from './types';
+import { GameState, GameActionTypes, SET_CURRENT_GAME } from './types';
 
+// TODO This structure is all wrong. we need to normalize the state tree
 export const initialState: GameState = {
   currentGameID: null,
 };
 
 export const gameReducer = (state = initialState, action: GameActionTypes) => {
-  return state;
+  switch (action.type) {
+    case SET_CURRENT_GAME:
+      return {
+        ...state,
+        currentGameID: action.payload.game,
+      };
+      break;
+    default:
+      return state;
+  }
 };
