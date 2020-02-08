ea6a966e6f2718e4f015653a9337fd0bfd3b1615
Camden Bickel <cam@bickel.io>
2020-02-08 15:29:28 -0500

    delete unused game logic stuff

--- server/store/games/types.ts
+++ server/store/games/types.ts
@@ -5,16 +5,3 @@ export enum GameID {
 export interface GameState {
   type: GameID; // e.g. The Resistance, fishbowl, etc
 }
-
-// ACTIONS
-
-export const SET_CURRENT_GAME = 'SET_CURRENT_GAME';
-
-export interface SetCurrentGameAction {
-  type: typeof SET_CURRENT_GAME;
-  payload: {
-    game: string;
-  };
-}
-
-export type GameActionTypes = SetCurrentGameAction;
