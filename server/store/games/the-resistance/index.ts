0b13a5c80b304f15ed88ab5979588bf03178526a
Camden Bickel <cam@bickel.io>
2020-02-11 14:55:09 -0500

    add role array

--- server/store/games/the-resistance/index.ts
+++ server/store/games/the-resistance/index.ts
@@ -7,18 +7,59 @@ import {
 import { GameType, GameID } from '../types';
 import { User } from 'store/general/types';
 import { pickRandomNumber } from '../../../utils/rng';
+import shuffleArray from '../../../utils/shuffle-array';
 
-const createPlayerFromUser = (user: User): ResistancePlayer => ({
-  role: Math.random() > 0.5 ? ResistanceRole.Resistance : ResistanceRole.Spy,
+const createPlayerFromUser = (
+  user: User,
+  role: ResistanceRole
+): ResistancePlayer => ({
+  role,
   userId: user.id,
   name: user.name,
 });
+
+const getNumberOfSpiesByPlayerCount = (playerCount: number): number => {
+  if (playerCount < 5) {
+    // this is not technically supported
+    return 1;
+  }
+
+  if (playerCount > 10) {
+    // this is not technically supported
+    return 5;
+  }
+
+  const counts: { [playerCount: number]: number } = {
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 3,
+    10: 4,
+  };
+
+  return counts[playerCount];
+};
 
 export const createNewGameOfTheResistance = (
   id: GameID,
   usersInRoom: User[]
 ): ResistanceGameState => {
-  const players: ResistancePlayer[] = usersInRoom.map(createPlayerFromUser);
+  const numSpies = getNumberOfSpiesByPlayerCount(usersInRoom.length);
+  const unshuffledRoleArray = [];
+  for (let i = 0; i < usersInRoom.length; i++) {
+    if (i < numSpies) {
+      unshuffledRoleArray.push(ResistanceRole.Spy);
+    } else {
+      unshuffledRoleArray.push(ResistanceRole.Resistance);
+    }
+  }
+  const roleArray = shuffleArray(unshuffledRoleArray);
+
+  const players: ResistancePlayer[] = usersInRoom.map((user, index) =>
+    createPlayerFromUser(user, roleArray[index])
+  );
+
   const missionLeader = players[pickRandomNumber(0, players.length - 1)];
 
   return {
---
+++ server/utils/shuffle-array.ts
@@ -0,0 +1,13 @@
+// https://stackoverflow.com/a/6274381/2399208
+function shuffleArray<T>(a: T[]): T[] {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+export default shuffleArray;
