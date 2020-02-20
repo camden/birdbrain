93522ba56bd81545a28836ec9aa61e449d5ba247
Camden Bickel <cam@bickel.io>
2020-02-19 23:20:52 -0500

    add more waiting messages

--- client/src/components/games/the-resistance/ShowMissionVotingResults.tsx
+++ client/src/components/games/the-resistance/ShowMissionVotingResults.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { sendMessage } from 'store/websocket/actions';
 import { rstAckMissionVoteResults } from '@server/store/games/the-resistance/actions';
 import Button from 'components/shared/button/Button';
+import WaitingMessage from './WaitingMessage';
 
 export interface ResistanceProps {
   game: ResistanceGameState;
@@ -25,6 +26,11 @@ const TheResistanceShowMissionVotingResults: React.FC<ResistanceProps> = ({
     ? game.missionFailVotes.length < 2
     : game.missionFailVotes.length === 0;
 
+  const allUsersWhoAcked = game.acknowledged;
+  const playersWhoStillNeedToAck = game.players.filter(
+    player => !allUsersWhoAcked.includes(player.userId)
+  );
+
   return (
     <div>
       <h2>The results are in!</h2>
@@ -40,6 +46,10 @@ const TheResistanceShowMissionVotingResults: React.FC<ResistanceProps> = ({
           : 'This mission has failed!'}
       </h2>
       {!acknowledged && <Button onClick={onContinueClick}>Continue</Button>}
+      <WaitingMessage
+        playersThatNeedToAct={playersWhoStillNeedToAck}
+        verb={'move on'}
+      />
     </div>
   );
 };
