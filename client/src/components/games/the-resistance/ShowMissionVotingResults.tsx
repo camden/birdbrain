86c2f8c8bc0d407fb6f8a7f3fe664cb5d6da6134
Camden Bickel <cam@bickel.io>
2020-02-20 00:09:13 -0500

    add cards with animation delay

--- client/src/components/games/the-resistance/ShowMissionVotingResults.tsx
+++ client/src/components/games/the-resistance/ShowMissionVotingResults.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
-import { ResistanceGameState } from '@server/store/games/the-resistance/types';
+import React, { useState, CSSProperties } from 'react';
+import {
+  ResistanceGameState,
+  ResistanceMissionVote,
+} from '@server/store/games/the-resistance/types';
 import { useDispatch } from 'react-redux';
 import { sendMessage } from 'store/websocket/actions';
 import { rstAckMissionVoteResults } from '@server/store/games/the-resistance/actions';
 import Button from 'components/shared/button/Button';
 import WaitingMessage from './WaitingMessage';
+import styles from './ShowMissionVotingResults.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/pro-solid-svg-icons';
+import cx from 'classnames';
+
+const ANIMATION_DELAY_MULTIPLIER = 1.5;
 
 export interface ResistanceProps {
   game: ResistanceGameState;
 }
+
+export interface VoteResultCardProps {
+  vote: ResistanceMissionVote;
+  index: number;
+}
+
+const VoteResultCard: React.FC<VoteResultCardProps> = ({ vote, index }) => {
+  const success = vote === ResistanceMissionVote.SUCCESS;
+
+  const style = {
+    '--appear-delay': `${(index + 1) * ANIMATION_DELAY_MULTIPLIER}s`,
+  } as CSSProperties;
+
+  return (
+    <div
+      className={cx(styles.vote_result_card, {
+        [styles.vote_result_card_success]: success,
+        [styles.vote_result_card_fail]: !success,
+      })}
+      style={style}
+    >
+      <FontAwesomeIcon icon={success ? faCheck : faTimes} />
+    </div>
+  );
+};
 
 const TheResistanceShowMissionVotingResults: React.FC<ResistanceProps> = ({
   game,
@@ -31,19 +65,27 @@ const TheResistanceShowMissionVotingResults: React.FC<ResistanceProps> = ({
     player => !allUsersWhoAcked.includes(player.userId)
   );
 
+  const allResults = game.missionSuccessVotes
+    .map(x => ResistanceMissionVote.SUCCESS)
+    .concat(game.missionFailVotes.map(x => ResistanceMissionVote.FAIL));
+
+  const resultSummaryStyle = {
+    '--appear-delay': `${(allResults.length + 1) *
+      ANIMATION_DELAY_MULTIPLIER}s`,
+  } as CSSProperties;
+
   return (
     <div>
       <h2>The results are in!</h2>
-      {game.missionSuccessVotes.map(vote => (
-        <div>Success</div>
-      ))}
-      {game.missionFailVotes.map(vote => (
-        <div>Fail</div>
-      ))}
-      <h2>
+      <div className={styles.vote_results}>
+        {allResults.map((vote, index) => (
+          <VoteResultCard vote={vote} index={index} />
+        ))}
+      </div>
+      <h2 className={styles.summary} style={resultSummaryStyle}>
         {missionSucceeded
-          ? 'This mission has succeeded!'
-          : 'This mission has failed!'}
+          ? `Mission ${game.mission} has succeeded!`
+          : `Mission ${game.mission} has failed!`}
       </h2>
       {!acknowledged && <Button onClick={onContinueClick}>Continue</Button>}
       <WaitingMessage
