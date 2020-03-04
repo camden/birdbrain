f583e2d9b6aa47fada50b5d52df91416f838d5af
Camden Bickel <cam@bickel.io>
2020-03-04 15:31:55 -0500

    style chat

--- client/src/components/games/chat/ChatMain.tsx
+++ client/src/components/games/chat/ChatMain.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import styles from './ChatMain.module.css';
 import { ChatGameState } from '@server/store/games/chat/types';
+import TextInput from 'components/shared/form/TextInput';
+import Button from 'components/shared/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/pro-solid-svg-icons';
 
 export interface SkullProps {
   game: ChatGameState;
@@ -8,12 +13,23 @@ export interface SkullProps {
 
 const ChatMain: React.FC<SkullProps> = ({ game }) => {
   return (
-    <div>
+    <div className={styles.chat}>
       <h1>Birdbrain Chat</h1>
-      <section>
+      <section className={styles.messages_list}>
         {game.messages.map(msg => (
           <div key={msg.id}>{msg.text}</div>
         ))}
+      </section>
+      <section className={styles.compose_area}>
+        <label htmlFor="chat-compose-input" className={styles.input_label}>
+          <FontAwesomeIcon
+            icon={faComment}
+            className={styles.input_label_icon}
+          />
+          Message
+        </label>
+        <TextInput id="chat-compose-input" className={styles.input} />
+        <Button secondary>Send</Button>
       </section>
     </div>
   );
