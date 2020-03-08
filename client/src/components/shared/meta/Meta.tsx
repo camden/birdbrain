22d234a4584f18821af731ed21a1d301df91fc76
Camden Bickel <cam@bickel.io>
2020-03-08 12:43:35 -0400

    move meta tags to index.html

--- client/src/components/shared/meta/Meta.tsx
+++ client/src/components/shared/meta/Meta.tsx
@@ -17,30 +17,6 @@ const Meta: React.FC<MetaProps> = () => {
         name="description"
         content="Play free local-multiplayer games with your friends!"
       />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={PUBLIC_URL} />
-      <meta property="og:title" content="Birdbrain Games" />
-      <meta
-        property="og:description"
-        content="Play free local-multiplayer games with your friends!"
-      />
-      <meta
-        property="og:image"
-        content={`${PUBLIC_URL}meta-tile-1200x628.png`}
-      />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={PUBLIC_URL} />
-      <meta property="twitter:title" content="Birdbrain Games" />
-      <meta
-        property="twitter:description"
-        content="Play free local-multiplayer games with your friends!"
-      />
-      <meta
-        property="twitter:image"
-        content={`${PUBLIC_URL}meta-tile-1200x628.png`}
-      ></meta>
     </Helmet>
   );
 };
