2335dfff39cc4ad656c1ef07ab42fd5807750f01
Camden Bickel <cam@bickel.io>
2020-03-25 12:33:09 -0700

    add randomized, even(ish) teams

--- server/store/games/fishbowl/index.ts
+++ server/store/games/fishbowl/index.ts
@@ -6,17 +6,21 @@ import {
 } from './types';
 import { GameID, GameType } from '../types';
 import { User } from 'store/general/types';
+import shuffleArray from 'utils/shuffle-array';
 
 export const DEFAULT_DURATION = 30500;
 export const QUICK_DEBUG_DURATION = 10500;
 
 export const ROUND_DURATION_MS = QUICK_DEBUG_DURATION;
 
-const createPlayerFromUser = (user: User): FishbowlPlayer => {
+const createPlayerFromUser = (
+  user: User,
+  team: FishbowlTeam
+): FishbowlPlayer => {
   return {
     name: user.name,
     userId: user.id,
-    team: FishbowlTeam.TEAM_A, // @TODO: RANDOMLY ASSIGN THIS
+    team,
   };
 };
 
@@ -24,7 +28,19 @@ export const createNewGameOfFishbowl = (
   id: GameID,
   usersInRoom: User[]
 ): FishbowlGameState => {
-  const players = usersInRoom.map(createPlayerFromUser);
+  const unshuffledTeamArray = [];
+  const maxOnOneTeam = Math.ceil(usersInRoom.length / 2);
+  for (let i = 0; i < usersInRoom.length; i++) {
+    if (i < maxOnOneTeam) {
+      unshuffledTeamArray.push(FishbowlTeam.TEAM_A);
+    } else {
+      unshuffledTeamArray.push(FishbowlTeam.TEAM_B);
+    }
+  }
+  const teamsArray = shuffleArray(unshuffledTeamArray);
+  const players = usersInRoom.map((user, index) =>
+    createPlayerFromUser(user, teamsArray[index])
+  );
 
   return {
     id,
