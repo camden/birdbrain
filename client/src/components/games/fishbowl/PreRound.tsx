f58e49d9ccf67963b1f43acd7ae5cbd7289abe5a
Camden Bickel <cam@bickel.io>
2020-04-02 14:27:56 -0700

    update styling for preround

--- client/src/components/games/fishbowl/PreRound.tsx
+++ client/src/components/games/fishbowl/PreRound.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, ReactNode } from 'react';
 import {
   FishbowlGameState,
   FishbowlGameType,
+  FishbowlTeam,
 } from '@server/store/games/fishbowl/types';
 import useSelector from 'store/use-selector';
 import { getCurrentUser } from 'store/selectors';
@@ -11,33 +12,37 @@ import { fshStartRound } from '@server/store/games/fishbowl/actions';
 import { sendMessage } from 'store/websocket/actions';
 import styles from './PreRound.module.css';
 import TeamBar from './TeamBar';
+import cx from 'classnames';
 
 export interface PreRoundProps {
   game: FishbowlGameState;
 }
+
+const getGameName = (gameType: FishbowlGameType): string => {
+  switch (gameType) {
+    case FishbowlGameType.TABOO:
+      return 'Taboo';
+    case FishbowlGameType.CHARADES:
+      return 'Charades';
+    case FishbowlGameType.PASSWORD:
+      return 'Password';
+  }
+};
 
 const getGameMessage = (gameType: FishbowlGameType): ReactNode => {
   switch (gameType) {
     case FishbowlGameType.TABOO:
       return (
         <>
-          This round is <strong>Taboo</strong>. Describe the phrase without
-          using any part of the phrase in your description!
+          Describe the phrase without using any part of the phrase in your
+          description!
         </>
       );
     case FishbowlGameType.CHARADES:
-      return (
-        <>
-          This round is <strong>Charades</strong>. Act out the phrase, and no
-          noises allowed!
-        </>
-      );
+      return <>Act out the phrase, and no noises allowed!</>;
     case FishbowlGameType.PASSWORD:
       return (
-        <>
-          This round is <strong>Password</strong>. Use only one word to describe
-          your phrase! This is the last round.
-        </>
+        <>Use only one word to describe your phrase! This is the last round.</>
       );
   }
 };
@@ -77,39 +82,35 @@ const PreRound: React.FC<PreRoundProps> = ({ game }) => {
 
   return (
     <div>
-      <TeamBar
-        teamName={currentPlayer.teamDisplayName}
-        playerName={currentPlayer.name}
-      />
-      <div className={styles.nextTurnInfo}>
-        It's <strong>{game.activePlayer.teamDisplayName}'s</strong> turn, and{' '}
-        <strong>{game.activePlayer.name}</strong> is up next.
-      </div>
-      {!isActivePlayer && isOnSameTeamAsActivePlayer && (
-        <div className={styles.info}>
-          <strong>{currentUser?.name}</strong>, you're on{' '}
-          <strong>{currentPlayer?.teamDisplayName}</strong>. Get ready!
+      <TeamBar team={currentPlayer.team} playerName={currentPlayer.name} />
+      <section>
+        <h2 className={styles.subtitle}>Up Next:</h2>
+        <div className={styles.nextPlayerName}>{game.activePlayer.name}</div>
+        <div
+          className={cx(styles.nextTeamName, {
+            [styles.teamA]: game.activePlayer.team === FishbowlTeam.TEAM_A,
+            [styles.teamB]: game.activePlayer.team === FishbowlTeam.TEAM_B,
+          })}
+        >
+          {game.activePlayer.teamDisplayName}
         </div>
-      )}
-      {!isActivePlayer && !isOnSameTeamAsActivePlayer && (
-        <div className={styles.info}>
-          <strong>{currentUser?.name}</strong>, you're on{' '}
-          <strong>{currentPlayer?.teamDisplayName}</strong>. Your team isn't
-          guessing this round.
+      </section>
+      <section className={styles.gameInfo}>
+        <h2 className={styles.subtitle}>Game Mode:</h2>
+        <div className={styles.gameModeName}>
+          {getGameName(game.currentGameType)}
         </div>
+        <div className={styles.gameModeDescription}>
+          {getGameMessage(game.currentGameType)}
+        </div>
+      </section>
+      {!isActivePlayer && (
+        <div>Waiting for {game.activePlayer.name} to start the round.</div>
       )}
-      <div className={styles.gameInfo}>
-        {getGameMessage(game.currentGameType)}
-      </div>
       {isActivePlayer && (
-        <>
-          <div className={styles.info}>
-            <strong>{currentUser?.name}</strong>, you're giving clues next.
-          </div>
-          <Button onClick={onStartRoundClick} fullWidth>
-            Start round
-          </Button>
-        </>
+        <Button onClick={onStartRoundClick} fullWidth>
+          Start round
+        </Button>
       )}
     </div>
   );
--- client/src/components/games/fishbowl/TeamBar.tsx
+++ client/src/components/games/fishbowl/TeamBar.tsx
@@ -3,16 +3,27 @@ import React from 'react';
 import styles from './TeamBar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUsers } from '@fortawesome/pro-solid-svg-icons';
+import { FishbowlTeam } from '@server/store/games/fishbowl/types';
+import {
+  TEAM_A_DISPLAY_NAME,
+  TEAM_B_DISPLAY_NAME,
+} from '@server/store/games/fishbowl';
+import cx from 'classnames';
 
 export interface TeamBarProps {
-  teamName: string;
+  team: FishbowlTeam;
   playerName: string;
 }
 
 const TeamBar: React.FC<TeamBarProps> = props => {
   return (
     <>
-      <div className={styles.wrapper}>
+      <div
+        className={cx(styles.wrapper, {
+          [styles.teamA]: props.team === FishbowlTeam.TEAM_A,
+          [styles.teamB]: props.team === FishbowlTeam.TEAM_B,
+        })}
+      >
         <div className={styles.inner}>
           <div className={styles.playerName}>
             <FontAwesomeIcon icon={faUser} className={styles.icon} />
@@ -20,7 +31,9 @@ const TeamBar: React.FC<TeamBarProps> = props => {
           </div>
           <div className={styles.teamName}>
             <FontAwesomeIcon icon={faUsers} className={styles.icon} />
-            {props.teamName}
+            {props.team === FishbowlTeam.TEAM_A
+              ? TEAM_A_DISPLAY_NAME
+              : TEAM_B_DISPLAY_NAME}
           </div>
         </div>
       </div>
