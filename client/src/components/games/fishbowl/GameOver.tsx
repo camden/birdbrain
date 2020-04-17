938953c5c6211c3ce1982acfd6671d854dddc4c9
Camden Bickel <cam@bickel.io>
2020-04-16 20:35:59 -0700

    tweaks

--- client/src/components/games/fishbowl/GameOver.tsx
+++ client/src/components/games/fishbowl/GameOver.tsx
@@ -13,6 +13,9 @@ import Button from 'components/shared/button/Button';
 import { useDispatch } from 'react-redux';
 import { sendMessage } from 'store/websocket/actions';
 import { endCurrentGame } from '@server/store/general/actions';
+import TeamBar from './TeamBar';
+import useSelector from 'store/use-selector';
+import { getCurrentUser } from 'store/selectors';
 
 export interface GameOverProps {
   game: FishbowlGameState;
@@ -20,6 +23,7 @@ export interface GameOverProps {
 
 const GameOver: React.FC<GameOverProps> = ({ game }) => {
   const dispatch = useDispatch();
+  const currentUser = useSelector(getCurrentUser());
 
   let resultMessage: ReactNode = '';
 
@@ -45,8 +49,18 @@ const GameOver: React.FC<GameOverProps> = ({ game }) => {
     );
   }
 
+  const currentPlayer = game.players.find((p) => p.userId === currentUser?.id);
+  if (!currentPlayer) {
+    return (
+      <div>
+        Something went wrong! Can't find player for current user. ERROR #2
+      </div>
+    );
+  }
+
   return (
     <div>
+      <TeamBar playerName={currentPlayer.name} team={currentPlayer.team} />
       <h1>Game over!</h1>
       <p>{resultMessage}</p>
       <section className={styles.scores}>
