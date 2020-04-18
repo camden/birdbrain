de2a5729f767e06b73ca2c193977657437209fd9
Camden Bickel <cam@bickel.io>
2020-04-18 13:35:08 -0700

    don't send state update unless the state changed

--- server/store/middleware/server-socket-middleware.ts
+++ server/store/middleware/server-socket-middleware.ts
@@ -2,6 +2,7 @@ import { MiddlewareAPI, Dispatch } from 'redux';
 import { GeneralActionTypes, RoomID } from '../general/types';
 import { RootState } from '..';
 import { getClientStateByRoomId } from '../general/selectors';
+import { equals } from 'ramda';
 
 const getSocketRoomId = (roomId: string) => {
   return `room:${roomId}`;
@@ -10,18 +11,26 @@ const getSocketRoomId = (roomId: string) => {
 const socketMiddleware = () => (socketServer: SocketIO.Server) => {
   const sendClientUpdate = (
     roomId: RoomID,
-    store: MiddlewareAPI<Dispatch, RootState>
+    store: MiddlewareAPI<Dispatch, RootState>,
+    previousState: RootState
   ) => {
     const clientState = getClientStateByRoomId(roomId)(store.getState());
 
-    socketServer
-      .to(getSocketRoomId(roomId))
-      .emit('client-state-update', clientState);
+    const prevClientState = getClientStateByRoomId(roomId)(previousState);
+
+    const changed = !equals(clientState, prevClientState);
+
+    if (changed) {
+      socketServer
+        .to(getSocketRoomId(roomId))
+        .emit('client-state-update', clientState);
+    }
   };
 
   return (store: MiddlewareAPI<Dispatch, RootState>) => (next: Dispatch) => (
     action: GeneralActionTypes
   ) => {
+    const previousState = { ...store.getState() };
     next(action);
 
     if (!('meta' in action)) {
@@ -29,7 +38,7 @@ const socketMiddleware = () => (socketServer: SocketIO.Server) => {
     }
 
     if (action.meta.sendClientUpdate) {
-      sendClientUpdate(action.meta.roomId, store);
+      sendClientUpdate(action.meta.roomId, store, previousState);
     }
   };
 };
