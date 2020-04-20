226fa05005adb5505361cc3e984ce1861dfe63af
Camden Bickel <cam@bickel.io>
2020-04-19 20:20:25 -0700

    spruce up the minigame results

--- client/src/components/games/ludum-dare/MinigameResults.tsx
+++ client/src/components/games/ludum-dare/MinigameResults.tsx
@@ -9,7 +9,11 @@ import { sendMessage } from 'store/websocket/actions';
 import { ludumAck } from '@server/store/games/ludum-dare/actions';
 import styles from './MinigameResults.module.css';
 import { useCurrentPlayer } from 'utils/ludum-dare-utils';
-import LudumCharacter from './Character';
+import LudumCharacter, { CharacterType, CharacterAnimation } from './Character';
+import { identity, times } from 'ramda';
+import LudumHeart from './Heart';
+import cx from 'classnames';
+import WaitingMessage from 'components/shared/WaitingMessage';
 
 export interface LudumMinigameResultsProps {
   game: LudumGameState;
@@ -31,17 +35,53 @@ const LudumMinigameResults: React.FC<LudumMinigameResultsProps> = ({
   const currentPlayerPassed = game.playersWhoPassedCurrentMinigame.includes(
     currentPlayer.userId
   );
+  const currentPlayerAcked = game.acknowledged.includes(currentPlayer.userId);
+  const playersWhoNeedToAck = game.players.filter(
+    (p) => !game.acknowledged.includes(p.userId)
+  );
+
+  const playerHealth = currentPlayerPassed
+    ? currentPlayer.health
+    : currentPlayer.health + 1;
 
   return (
     <div className={styles.wrapper}>
-      <div>nice the game is over. good job</div>
-      <LudumCharacter id={currentPlayer.character.id} />
-      <p>
-        these players were successful:{' '}
-        {playersWhoPassed.map((p) => p.name).join(', ')}
-      </p>
+      <h1>{currentPlayerPassed ? 'You did it!' : 'You failed.'}</h1>
+      <LudumCharacter
+        id={currentPlayer.character.id}
+        type={currentPlayerPassed ? CharacterType.WIN : CharacterType.LOSE}
+        typeWhenPressed={
+          currentPlayerPassed ? CharacterType.WIN : CharacterType.NERVOUS
+        }
+        animation={
+          currentPlayerPassed
+            ? CharacterAnimation.SWAY
+            : CharacterAnimation.SHAKE
+        }
+      />
+      <div className={styles.heartsContainer}>
+        {times(identity, playerHealth).map((idx) => (
+          <div
+            key={idx}
+            className={cx(styles.heartWrapper, {
+              [styles.missingHeart]:
+                !currentPlayerPassed && idx === playerHealth - 1,
+            })}
+          >
+            <LudumHeart className={styles.heart} />
+          </div>
+        ))}
+      </div>
       <div className={styles.footer}>
-        <Button onClick={onContinueClick}>Continue</Button>
+        {currentPlayerAcked && (
+          <WaitingMessage
+            playersThatNeedToAct={playersWhoNeedToAck.map((p) => p.name)}
+            verb={'move on'}
+          />
+        )}
+        {!currentPlayerAcked && (
+          <Button onClick={onContinueClick}>Continue</Button>
+        )}
       </div>
     </div>
   );
